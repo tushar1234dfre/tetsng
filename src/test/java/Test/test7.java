2412852a17fccad591e3137e4543c9db31d7b98f
tushar1234dfre <tusharphatangare1999@gmail.com>
2023-07-18 15:41:47 +0530

    test7 commit

---
+++ src/test/java/Test/test7.java
@@ -0,0 +1,39 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class test7 {
+	
+	@Test
+	  public void Weblogin()
+	  {
+		  System.out.println("webLogin");
+		  
+		  
+		  
+	  }
+	  @Test
+	  public void Websignup()
+	  {
+		  System.out.println("Websignup");
+	  }
+	  @Test
+	  public void Mobileandroid()
+	  {
+		  System.out.println("Mobile Android");
+	  }
+	  @Test
+	  public void Mobileios()
+	  {
+
+		  System.out.println("Mobile IOS");
+	  }
+	  @Test
+	  public void Mobileapp()
+	  {
+		  System.out.println("Mobile App");  
+	  }
+	  
+	  }
+
+
